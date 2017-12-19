985c7232a2d29dd74c5c5497416f5ea65fa4b776
周闽强 <zhouminqiang@kuainiugroup.com>
2017-12-19 09:37:05 +0800

    navigation click to move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,25 @@ using UnityEngine.AI;
 public class Player : MonoBehaviour {
 
     private NavMeshAgent agent;
-    public Transform target;
+    //public Transform target;
 
 
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
-        agent.destination = target.position;
+        //agent.destination = target.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
 	}
 }
